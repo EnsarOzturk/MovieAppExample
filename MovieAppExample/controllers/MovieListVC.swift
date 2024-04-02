f8feb96f9e7241950335eb77d4053e56acaa197b
Ensar Öztürk <ozturkkensar@gmail.com>
2024-04-02 12:13:29 +0300

    integrated ViewModel for movieListVC

--- MovieAppExample/controllers/MovieListVC.swift
+++ MovieAppExample/controllers/MovieListVC.swift
@@ -10,110 +10,81 @@ import UIKit
 final class MovieListVC: UIViewController {
     
     @IBOutlet weak var collectionView: UICollectionView!
-    private let networkManager = NetworkManager()
-    private var movies: [Movie] = []
-    private var filteredMovies: [Movie] = []
-    private var isSearching: Bool = false
-    private var viewStyle: viewStyle = .small
+    private var viewModel: MovieListViewModel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        searchBar()
-        contentInset()
-        collectionView.delegate = self
-        collectionView.dataSource = self
-        collectionView.register(UINib(nibName: "MovieListCell", bundle: nil), forCellWithReuseIdentifier: MovieListCell.identifier)
-        collectionView.register(UINib(nibName: "BigCardCell", bundle: nil), forCellWithReuseIdentifier: BigCardCell.identifier)
-        navigationController?.navigationBar.prefersLargeTitles = true
-        title = "MovieList"
         
-        networkManager.request(type: MovieResponse.self, item: DiscoverMovieEndpointItem()) { result in
-            switch result {
-            case .success(let response):
-                if let movies =  response.results {
-                    self.movies = movies
-                    DispatchQueue.main.async {
-                        self.collectionView.reloadData()
-                    }
-                }
-            case .failure(let error): print(error)
-            }
-        }
+        viewModel = MovieListViewModel(networkManager: NetworkManager())
+        searchBar()
+        setupCollectionView()
+        fetchMovies()
+        contentInset()
     }
-    
-    private func contentInset() {
-        switch viewStyle {
-        case .small:
-             return collectionView.contentInset = UIEdgeInsets(top: 16, left: 16, bottom: 16, right: 16)
-        case .big:
-            return collectionView.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
-        }
-    }
-    
+
     private func searchBar() {
         let searchController = UISearchController(searchResultsController: nil)
         searchController.searchResultsUpdater = self
         navigationItem.searchController = searchController
+        navigationItem.hidesSearchBarWhenScrolling = false // Search bar'ın scroll ederken saklanmaması için
         searchController.searchBar.placeholder = "Search Movies"
-        searchController.definesPresentationContext = false
+        searchController.definesPresentationContext = true // Bu özelliği true olarak ayarlayın
+    }
+    
+    private func setupCollectionView() {
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.register(UINib(nibName: "MovieListCell", bundle: nil), forCellWithReuseIdentifier: MovieListCell.identifier)
+        collectionView.register(UINib(nibName: "BigCardCell", bundle: nil), forCellWithReuseIdentifier: BigCardCell.identifier)
+    }
+    
+    private func fetchMovies() {
+        viewModel.fetchMovies { [weak self] result in
+            switch result {
+            case .success:
+                self?.collectionView.reloadData()
+            case .failure(let error):
+                print("Error fetching movies: \(error)")
+            }
+        }
     }
     
     @IBAction func viewStyleButtonTapped(_ sender: UIBarButtonItem) {
-        viewStyle = viewStyle == .small ? .big : .small
+        viewModel.toggleViewStyle()
         collectionView.reloadData()
     }
-    
-    private func butonItemImage() {
-        let buttonImage: UIImage?
-        if viewStyle == .small {
-            buttonImage = UIImage(named: "rectangle.split.1x2.fill")
-        } else {
-            buttonImage = UIImage(named: "rectangle.split.2x2.fill")
-        }
-        navigationItem.rightBarButtonItem?.image = buttonImage
+}
+
+    private func contentInset() {
+        switch viewModel.viewStyle {
+        case .small:
+         collectionView.contentInset = UIEdgeInsets(top: 16, left: 16, bottom: 16, right: 16)
+        case .big:
+        collectionView.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
     }
 }
 
 extension MovieListVC: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        if isSearching {
-            return filteredMovies.count
-        } else {
-            return movies.count
-        }
-        collectionView.reloadData()
+        return viewModel.numberOfMovies
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        switch viewStyle {
-        case .small:
-            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: MovieListCell.identifier, for: indexPath) as! MovieListCell
-            let movie: Movie
-            if isSearching {
-                movie = filteredMovies[indexPath.row]
-            } else {
-                movie = movies[indexPath.row]
-            }
-            cell.configure(movie: movie)
-            return cell
-        case .big:
-            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: BigCardCell.identifier, for: indexPath) as! BigCardCell
-            let movie: Movie
-            if isSearching {
-                movie = filteredMovies[indexPath.row]
-            } else {
-                movie = movies[indexPath.row]
-            }
-            cell.configure(movie: movie)
-            
-            return cell
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: viewModel.viewStyle == .small ? MovieListCell.identifier : BigCardCell.identifier, for: indexPath)
+        
+        if let movieCell = cell as? MovieListCell {
+            movieCell.configure(movie: viewModel.movie(at: indexPath.row))
+        } else if let bigCardCell = cell as? BigCardCell {
+            bigCardCell.configure(movie: viewModel.movie(at: indexPath.row))
         }
+        
+        return cell
     }
 }
 
 extension MovieListVC: UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        switch viewStyle {
+        switch viewModel.viewStyle {
         case .small:
             let width = (UIScreen.main.bounds.width - 48) / 2
             let height = width / 2 * 3
@@ -124,9 +95,9 @@ extension MovieListVC: UICollectionViewDelegateFlowLayout {
             return CGSize(width: width, height: height)
         }
     }
-    
+
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
-        switch viewStyle {
+        switch viewModel.viewStyle {
         case .small:
             return 16
         case .big:
@@ -137,30 +108,20 @@ extension MovieListVC: UICollectionViewDelegateFlowLayout {
 
 extension MovieListVC: UICollectionViewDelegate {
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        let selectedMovie = movies[indexPath.row].id
+        let selectedMovieId = viewModel.movie(at: indexPath.row).id
         let detailVC = storyboard?.instantiateViewController(withIdentifier: "MovieDetail") as! MovieDetailVC
-        detailVC.movieId = selectedMovie!
+        detailVC.movieId = selectedMovieId!
         navigationController?.pushViewController(detailVC, animated: true)
     }
 }
 
 extension MovieListVC: UISearchResultsUpdating {
     func updateSearchResults(for searchController: UISearchController) {
-        if searchController.searchBar.text?.isEmpty ?? true {
-            filteredMovies.removeAll()
-            isSearching = false
-        } else {
-            isSearching = true
-            filteredMovies = movies.filter { $0.title!.lowercased().contains(searchController.searchBar.text?.lowercased() ?? "") }
-        }
+        viewModel.filterMovies(with: searchController.searchBar.text ?? "")
         collectionView.reloadData()
     }
 }
 
-enum viewStyle {
-    case small
-    case big
-}
 
 
 
--- MovieAppExample/model/MovieListViewModel.swift
+++ MovieAppExample/model/MovieListViewModel.swift
@@ -6,3 +6,60 @@
 //
 
 import Foundation
+
+class MovieListViewModel {
+    
+    private let networkManager: NetworkManager
+    private var movies: [Movie] = []
+    private var filteredMovies: [Movie] = []
+   
+    var viewStyle: ViewStyle = .small
+    
+    var numberOfMovies: Int {
+        return isSearching ? filteredMovies.count : movies.count
+    }
+    
+    init(networkManager: NetworkManager) {
+        self.networkManager = networkManager
+    }
+    
+    func fetchMovies(completion: @escaping (Result<Void, Error>) -> Void) {
+        networkManager.request(type: MovieResponse.self, item: DiscoverMovieEndpointItem()) { result in
+            switch result {
+            case .success(let response):
+                if let fetchedMovies =  response.results {
+                    self.movies = fetchedMovies
+                    completion(.success(()))
+                }
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        }
+    }
+    
+    func movie(at index: Int) -> Movie {
+        return isSearching ? filteredMovies[index] : movies[index]
+    }
+    
+    func filterMovies(with searchText: String) {
+        if searchText.isEmpty {
+            filteredMovies.removeAll()
+        } else {
+            filteredMovies = movies.filter { $0.title?.lowercased().contains(searchText.lowercased()) ?? false }
+        }
+    }
+    
+    var isSearching: Bool {
+        return !filteredMovies.isEmpty
+    }
+    
+    func toggleViewStyle() {
+        viewStyle = (viewStyle == .small) ? .big : .small
+    }
+}
+
+enum ViewStyle {
+    case small
+    case big
+}
+
